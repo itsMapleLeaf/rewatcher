a1fc129a32031788c18e22c6259801ede4db1a70
just nobody <kingdaro@gmail.com>
2016-12-13 16:38:02 -0500

    some cleanup

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
@@ -23,37 +24,53 @@ main(List<String> args) async {
   print('Running "$program ${arguments.join(' ')}"');
   process = await runProgram(program, arguments);
 
-  watcher(FileSystemEvent event) async {
-    if (event is FileSystemCreateEvent) {
-      print('Created: ${event.path}');
-    } else if (event is FileSystemModifyEvent) {
-      print('Modified: ${event.path}');
-    } else if (event is FileSystemDeleteEvent) {
-      print('Deleted: ${event.path}');
-    } else if (event is FileSystemMoveEvent) {
-      print('Moved: ${event.path} -> ${event.destination}');
-    }
-
-    if (process != null) {
-      process.kill();
-      process = null;
-      process = await runProgram(program, arguments);
-    }
-  }
-
   for (final path in watchPaths) {
-    if (await FileSystemEntity.isFile(path)) {
-      if (!Platform.isWindows) {
-        new File(path).watch(recursive: true).listen(watcher);
-      }
-    } else if (await FileSystemEntity.isDirectory(path)) {
-      new Directory(path).watch(recursive: true).listen(watcher);
+    try {
+      print('Watching $path');
+      final watcher = await createWatcher(path);
+      watcher.listen((event) async {
+        logWatchOutput(event);
+        if (process != null) {
+          process.kill();
+          process = null;
+          process = await runProgram(program, arguments);
+        }
+      });
+    } catch(err) {
+      print(err);
+      return;
     }
   }
 }
 
-runProgram(String program, List<String> arguments) async {
+Future<Process> runProgram(String program, List<String> arguments) async {
   return await Process.start(program, arguments)
     ..stdout.transform(UTF8.decoder).listen(print)
     ..stderr.transform(UTF8.decoder).listen(print);
+}
+
+Future<Stream<FileSystemEvent>> createWatcher(String path) async {
+  if (await FileSystemEntity.isFile(path)) {
+    if (!Platform.isWindows) {
+      throw "Couldn't watch $path: Watching files isn't supported on this system. Sorry :(";
+    } else {
+      return new File(path).watch();
+    }
+  } else if (await FileSystemEntity.isDirectory(path)) {
+    return new Directory(path).watch();
+  } else {
+    throw "$path must be a file or a directory.";
+  }
+}
+
+void logWatchOutput(FileSystemEvent event) {
+  if (event is FileSystemCreateEvent) {
+    print('Created: ${event.path}');
+  } else if (event is FileSystemModifyEvent) {
+    print('Modified: ${event.path}');
+  } else if (event is FileSystemDeleteEvent) {
+    print('Deleted: ${event.path}');
+  } else if (event is FileSystemMoveEvent) {
+    print('Moved: ${event.path} -> ${event.destination}');
+  }
 }
